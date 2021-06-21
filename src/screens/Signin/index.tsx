ee6a484caf446d08b9075711a50cf1ccf40b591b
Matheus Dantas <matheusdantas@ad.corp.google.com>
2021-06-21 16:49:43 -0300

    first commit

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import {styles} from './style';
+
+export default function SignIn(){
+    return(
+        <Text>
+            Hello world
+        </Text>
+    )
+}
